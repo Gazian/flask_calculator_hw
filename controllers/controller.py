6fc581257cc27ca620e101ec0ea80905ef176b59
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-27 22:33:45 +0100

    adding progress so far - calculator works but I have bypassed the .html templates.  Have an idea that might work - will try tomorrow.

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,5 +2,20 @@ from flask import render_template
 from app import app
 from modules.calculator import *
 
-@app.route('/add')
-def
+@app.route('/calculator') 
+
+@app.route('/add/<value_1>/<value_2>')
+def add(value_1,value_2):
+    return f"The answer is {int(value_1)+int(value_2)}"
+
+@app.route('/subtract/<value_1>/<value_2>')
+def subtract(value_1,value_2):
+    return f"The answer is {int(value_1)-int(value_2)}"
+
+@app.route('/multiply<value_1>/<value_2>')
+def multiply (value_1, value_2):
+    return f"The answer is {int(value_1)*int(value_2)}"
+
+@app.route('/divide')
+def divide (value_1,value_2):
+    return f"The answer is {int(value_1)/int(value_2)}"
