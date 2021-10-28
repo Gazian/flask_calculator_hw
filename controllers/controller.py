d04fa4b7c9bcd8edeed9503d3fab7d09a6f2e4cd
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-28 09:35:02 +0100

    Adding solution that uses functions instead of the calculations being hard-coded into the controller file

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,20 +2,22 @@ from flask import render_template
 from app import app
 from modules.calculator import *
 
+
+
 @app.route('/calculator') 
 
 @app.route('/add/<value_1>/<value_2>')
 def add(value_1,value_2):
-    return f"The answer is {int(value_1)+int(value_2)}"
+    return f"The answer is {calc_add_function(int(value_1),int(value_2))}"
 
 @app.route('/subtract/<value_1>/<value_2>')
 def subtract(value_1,value_2):
-    return f"The answer is {int(value_1)-int(value_2)}"
+    return f"The answer is {calc_subtract_function(int(value_1),int(value_2))}"
 
-@app.route('/multiply<value_1>/<value_2>')
+@app.route('/multiply/<value_1>/<value_2>')
 def multiply (value_1, value_2):
-    return f"The answer is {int(value_1)*int(value_2)}"
+    return f"The answer is {calc_multiply_function(int(value_1),int(value_2))}"
 
-@app.route('/divide')
+@app.route('/divide/<value_1>/<value_2>')
 def divide (value_1,value_2):
-    return f"The answer is {int(value_1)/int(value_2)}"
+    return f"The answer is {calc_divide_function(int(value_1),int(value_2))}"
